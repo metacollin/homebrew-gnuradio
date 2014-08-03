e1f5b5d9ea70b6fc2ff6962c54b5676f3bdbf82a
metacollin <metacollin@gmail.com>
2014-08-03 15:26:58 -0600

    Refined.

--- gnuradio.rb
+++ gnuradio.rb
@@ -4,7 +4,11 @@ class Gnuradio < Formula
   homepage "http://gnuradio.org"
   url "http://jenkins.gnuradio.org/builds/gnuradio-jenkins-GNURadio-master-82-0-g93db96fa-2014-07-21.tar.gz"
   sha1 "08c4ee3fc30dd3c3ccd097cccd8efff7971e6feb"
-
+  head "https://github.com/gnuradio/gnuradio.git"
+  
+  option 'with-qt', 'Build with exta GUI features that use QT'
+  option 'with-docs', 'Build gnuradio documentation using sphinx.'
+  
   depends_on "cmake" => :build
   depends_on 'Cheetah' => :python
   depends_on 'lxml' => :python
@@ -21,31 +25,39 @@ class Gnuradio < Formula
   depends_on 'sdl'
   depends_on 'libusb'
   depends_on 'orc'
-  depends_on 'pyqt' #if ARGV.include?('--with-qt')
-  depends_on 'pyqwt' #if ARGV.include?('--with-qt')
-  depends_on 'doxygen'# if ARGV.include?('--with-docs')
-  depends_on 'sphinx' #if ARGV.include?('--with-docs')
-  depends_on 'wxmac'
+  depends_on 'pyqt' => [:optional, "with-qt"]
+  depends_on 'pyqwt' => [:optional, "with-qt"]
+  #depends_on 'doxygen' => [:optional, "with-docs"]
+  depends_on 'sphinx' => [:optional, "with-docs"]
+  depends_on 'wxmac' 
   depends_on 'wxpython'
   depends_on 'wxwidgets'
-
-
- # def options
-    [
-  #    ['--with-qt', 'Build gr-qtgui.'],
-  #    ['--with-docs', 'Build docs.']
-    ]
-#  end
   
 
   def install
     ENV['CMAKE_C_COMPILER'] = '/usr/bin/llvm-gcc'
-    ENV['CMAKE_CXX_COMPILER'] = 'CXX=/usr/bin/llvm-g++'
+    ENV['CMAKE_CXX_COMPILER'] = '/usr/bin/llvm-g++'
     
     mkdir 'build' do
-      args = ["-DCMAKE_PREFIX_PATH=#{prefix}", "-DPYTHON_EXECUTABLE=/usr/local/Cellar/python/2.7.8/bin/python", "-DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python/2.7.8/Frameworks/Python.framework/Headers", "-DSPHINX_EXECUTEABLE=/usr/local/bin/rst2html.py", "-DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.8/Frameworks/Python.framework/Versions/2.7/Python"] + std_cmake_args
-     # args << '-DENABLE_GR_QTGUI=OFF' unless ARGV.include?('--with-qt')
-      #args << '-DENABLE_DOXYGEN=OFF' unless ARGV.include?('--with-docs')
+      args = %W[
+        -DCMAKE_PREFIX_PATH=#{prefix}
+        -DPYTHON_EXECUTABLE=/usr/local/Cellar/python/2.7.8/bin/python
+        -DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python/2.7.8/Frameworks/Python.framework/Headers
+        -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.8/Frameworks/Python.framework/Versions/2.7/Python
+        ] + std_cmake_args
+
+      if build.with? "docs"
+        args << "-DSPHINX_EXECUTEABLE=/usr/local/bin/rst2html.py"
+      else
+        args << "-DENABLE_SPHINX=OFF"
+      end
+      
+      if build.with? "qt"
+        args << "-DENABLE_GR_QTGUI=ON"
+      else
+        args << "-DENABLE_GR_QTGUI=OFF"
+      end
+    end
       
     
     system "cmake", "..", *args
