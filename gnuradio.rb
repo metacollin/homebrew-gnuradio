34c865b4f06b61da19cb852b5e55c274000565cf
metacollin <metacollin@gmail.com>
2014-11-30 03:57:40 -0700

    Merged in dhom's style fixes and python bundling
    Thanks again!

--- gnuradio.rb
+++ gnuradio.rb
@@ -8,7 +8,34 @@ class Gnuradio < Formula
 
   option "with-qt", "Build with QT widgets in addition to wxWidgets"
   option "with-docs", "Build gnuradio documentation"
+  option "with-brewed-python", "Use the Homebrew version of Python"
 
+  resource "docutils" do
+    url "https://pypi.python.org/packages/source/d/docutils/docutils-0.12.tar.gz"
+    sha1 "002450621b33c5690060345b0aac25bc2426d675"
+  end
+
+  resource "Cheetah" do
+    url "https://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz"
+    sha1 "c218f5d8bc97b39497680f6be9b7bd093f696e89"
+  end
+
+  resource "lxml" do
+    url "https://pypi.python.org/packages/source/l/lxml/lxml-3.4.1.tar.gz"
+    sha1 "c09f4e8e71fc9d49fb43bf33821da816ce887396"
+  end
+
+  resource "numpy" do
+    url "http://downloads.sourceforge.net/project/numpy/NumPy/1.9.1/numpy-1.9.1.tar.gz"
+    sha1 "a96ddd221b34c08f08ae700a51969ddeb17d40ea"
+  end
+
+  resource "scipy" do
+    url "http://downloads.sourceforge.net/project/scipy/scipy/0.14.0/scipy-0.14.0.tar.gz"
+    sha1 "faf16ddf307eb45ead62a92ffadc5288a710feb8"
+  end
+
+  depends_on :fortran => :build
   depends_on "cmake" => :build
   depends_on "matplotlib" => :python
   depends_on "boost"
@@ -27,6 +54,22 @@ class Gnuradio < Formula
   depends_on "wxmac"
 
   def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    python_args = ["install", "--prefix=#{libexec}"]
+    %w[Cheetah lxml].each do |r|
+      resource(r).stage { system "python", "setup.py", *python_args }
+    end
+    python_fortran_args = ["build", "--fcompiler=gnu95", *python_args]
+    %w[numpy scipy].each do |r|
+      resource(r).stage { system "python", "setup.py", *python_fortran_args }
+    end
+
+    if build.with? "docs"
+      resource("docutils").stage do
+        system "python", "setup.py", "install", "--prefix=#{buildpath}"
+      end
+    end
+
     mkdir "build" do
       ENV["CMAKE_C_COMPILER"] = "#{ENV.cc}"
       ENV["CMAKE_CXX_COMPILER"] = "#{ENV.cxx}"
@@ -34,11 +77,13 @@ class Gnuradio < Formula
       args = %W[
         -DCMAKE_PREFIX_PATH=#{prefix}
         -DENABLE_DOXYGEN=Off
-        -DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'
-        ] + std_cmake_args
+      ]
+      if build.with? "brewed-python"
+        args << "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'"
+      end
 
       if build.with? "docs"
-        args << "-DSPHINX_EXECUTEABLE=/usr/local/bin/rst2html.py"
+        args << "-DSPHINX_EXECUTEABLE=#{buildpath}/bin/rst2html.py"
       else
         args << "-DENABLE_SPHINX=OFF"
       end
@@ -49,9 +94,13 @@ class Gnuradio < Formula
         args << "-DENABLE_GR_QTGUI=OFF"
       end
 
-      system "cmake", "..", *args
+      system "cmake", "..", *args, *std_cmake_args
       system "make"
       system "make install"
+
+      inreplace "#{prefix}/etc/gnuradio/conf.d/grc.conf" do |s|
+        s.gsub! "#{prefix}/", "#{HOMEBREW_PREFIX}/"
+      end
     end
   end
 end
