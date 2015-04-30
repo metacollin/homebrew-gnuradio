626f1ae60e29ba1eba7f0ffdab059f5b28c44cac
Collin Anderson <collinanderson@flamingpenguin.metacollin.net>
2015-04-30 14:14:42 -0600

    Tons of fixes, reworked python handling.

--- README.md
+++ README.md
@@ -1,20 +1,71 @@
 homebrew-gnuradio
 =================
-**Now maintained and fixed as of 2/1/15. Sorry about the haitus.**
+Delicious and foamy tap of the latest development version of gnuradio! Yay! 
 
-Delicious and foamy tap of the latest development version of gnuradio! Yay! Builds clean on Mac OS X Yosemite 10.10 using Xcode 6.1 command line tools (clang/llvm).
+This will *not* automatically handle gnuradio's python dependencies, in accordance with homebrew's python 
+policy. Typically, this can be done using pip:
 
-Simply run: 
+```sh
+pip install Cheetah lxml matplotlib numpy scipy docutils
+```
+
+Then, simply run: 
 ```sh
 brew tap metacollin/gnuradio
 brew install gnuradio
 ```
-or, if you prefer to use brewed python instead of the system's python:
-```sh
-brew install gnuradio --with-brewed-python
-```
 
-Other available flags to be used at your discretion are `--without-qt` and `--without-docs`. **Note:** Disabling QT will prevent the GUI companion from being built.
+But don't worry, if you weren't able to use pip or are missing a python dependency, homebrew will see which 
+python packages are missing (if any) and tell you how to install them after you try to `brew install gnuradio`.
+
+There is an alternative however.  One can use the `--with-python-deps` flag and the formula will handle all 
+python dependencies automatically for you, and will build them specific for gnuradio, leaving your system wide 
+python setup untouched.  The downside to this is those python libaries will *not* be available systemwide, which 
+may cause issues if you are developing your own blocks etc.
+
+Other available flags to be used at your discretion are `--without-qt` and `--without-docs`. **Note:** Disabling 
+QT will prevent the GUI companion from being built.
 
 The latest dev build can be attempted using the `--HEAD` argument, but this isn't necessarily guaranteed to 
 work.
+
+Python Woes and Other Issues
+-------------
+
+If you are experiencing problems such as gnuradio-companion not being built, or gnuradio crashing immediately on 
+launch, or simple build failure, it's almost certainly a python issue.  
+
+Here is the problem: homebrew bottles are linked against homebrew's python formula, but it wasn't always this 
+way.  Certain dependencies like pygtk or pyqt, if they've been on your system long enough, may have been built 
+locally and will have linked to OS X's python, while other gnuradio dependencies are linking to homebrew's 
+python.  One must link to either OS X's python, OR homebrew's python, but never both in the same executable 
+binary.
+
+The only fool-proof way to ensure this is by reinstalling all of the gnuradio dependencies:
+
+```ssh
+brew rm $(brew deps gnuradio)
+brew rm gnuradio
+brew install gnuradio
+```
+
+To make matters even more complicated, if you hadn't been using homebrew's python before, you might have 
+installed the python dependencies with pip under OS X's python, and haven't been installed for homebrew's 
+python.  The formula may complain about missing python dependencies, please rerun the commands it asks, now that 
+it will run homebrew python's pip.  If it doesn't find any issues, that is OK too, again, it may or may not 
+happen depending on your system.
+
+
+Finally, you might be wondering, "What if i don't WANT to use homebrew's python? I want one python on my system 
+so I don't have to deal with this!"  No problem! Homebrew only uses it's own python when using bottles.  If you 
+run:
+
+```ssh
+brew rm $(brew deps gnuradio)
+brew rm gnuradio
+brew install gnuradio --build-from-source  # Important! Rebuilds all dependencies against OS X's python!
+```
+
+Again, you may need to run pip as needed, but its ok if the formula never asks either.  The downside to this is 
+that you won't be using bottles, so build times will be significant, and could potentially break other formula 
+that were built against homebrew's python and they will need to be rebuilt from source to fix.  
--- gnuradio.rb
+++ gnuradio.rb
@@ -8,50 +8,58 @@ class Gnuradio < Formula
 
   option "without-qt", "Build with QT widgets in addition to wxWidgets"
   option "without-docs", "Build gnuradio documentation"
-  option "with-brewed-python", "Use the Homebrew version of Python"
+  option "with-python-deps", "Will handle python dependencies for you. Deprecated."
 
-  resource "docutils" do
-    url "https://pypi.python.org/packages/source/d/docutils/docutils-0.12.tar.gz"
-    sha1 "002450621b33c5690060345b0aac25bc2426d675"
-  end
+  if build.with? "python-deps"
+    resource "docutils" do
+      url "https://pypi.python.org/packages/source/d/docutils/docutils-0.12.tar.gz"
+      sha1 "002450621b33c5690060345b0aac25bc2426d675"
+    end
 
-  resource "Cheetah" do
-    url "https://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz"
-    sha1 "c218f5d8bc97b39497680f6be9b7bd093f696e89"
-  end
+    resource "Cheetah" do
+      url "https://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz"
+      sha1 "c218f5d8bc97b39497680f6be9b7bd093f696e89"
+    end
 
-  resource "lxml" do
-    url "https://pypi.python.org/packages/source/l/lxml/lxml-3.4.1.tar.gz"
-    sha1 "c09f4e8e71fc9d49fb43bf33821da816ce887396"
-  end
+    resource "lxml" do
+      url "https://pypi.python.org/packages/source/l/lxml/lxml-3.4.1.tar.gz"
+      sha1 "c09f4e8e71fc9d49fb43bf33821da816ce887396"
+    end
 
-  resource "numpy" do
-    url "http://downloads.sourceforge.net/project/numpy/NumPy/1.9.1/numpy-1.9.1.tar.gz"
-    sha1 "a96ddd221b34c08f08ae700a51969ddeb17d40ea"
-  end
+    resource "numpy" do
+      url "http://downloads.sourceforge.net/project/numpy/NumPy/1.9.1/numpy-1.9.1.tar.gz"
+      sha1 "a96ddd221b34c08f08ae700a51969ddeb17d40ea"
+    end
 
-  resource "scipy" do
-    url "http://downloads.sourceforge.net/project/scipy/scipy/0.14.0/scipy-0.14.0.tar.gz"
-    sha1 "faf16ddf307eb45ead62a92ffadc5288a710feb8"
-  end
+    resource "scipy" do
+      url "http://downloads.sourceforge.net/project/scipy/scipy/0.14.0/scipy-0.14.0.tar.gz"
+      sha1 "faf16ddf307eb45ead62a92ffadc5288a710feb8"
+    end
 
-  if build.without? "brewed-python"
     resource "matplotlib" do
       url "https://downloads.sourceforge.net/project/matplotlib/matplotlib/matplotlib-1.4.2/matplotlib-1.4.2.tar.gz"
       sha1 "242c57ddae808b1869cad4b08bb0973c513e12f8"
     end
   end
 
-  depends_on :fortran => :build
-  depends_on "cmake" => :build
-  if build.with? "brewed-python"
-    deponds_on "matplotlib" => :python
+  depends_on :python
+  if build.without? "python-deps"
+    depends_on "Cheetah" => :python
+    depends_on "lxml" => :python
+    depends_on "matplotlib" => :python
+    depends_on "numpy" => :python
+    depends_on "scipy" => :python
+    depends_on "docutils" => :python
   end
+  depends_on :fortran => :build
+  depends_on "swig" => :build
+  depends_on "cmake" => :build
   depends_on "boost"
   depends_on "cppunit"
   depends_on "gsl"
   depends_on "fftw"
-  depends_on "swig" => :build
+  depends_on "sip"
+  depends_on "pygobject"
   depends_on "pygtk"
   depends_on "sdl"
   depends_on "libusb"
@@ -64,44 +72,47 @@ class Gnuradio < Formula
   depends_on "freetype"
 
   def install
-    ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
-    python_args = ["install", "--prefix=#{libexec}"]
-    %w[Cheetah lxml].each do |r|
-      resource(r).stage { system "python", "setup.py", *python_args }
-    end
-    if build.without? "brewed-python"
-      resource("matplotlib").stage do
-        if MacOS.version >= :yosemite
-          inreplace "setupext.py", "'freetype2', 'ft2build.h',", "'freetype2', 'freetype2/ft2build.h',"
-        end
-        system "python", "setup.py", *python_args
+    if build.with? "python-deps"
+      ENV.prepend_create_path "PYTHONPATH", "#{libexec}/lib/python2.7/site-packages"
+      python_args = ["install", "--prefix=#{libexec}"]
+      %w[Cheetah lxml].each do |r|
+        resource(r).stage { system "python", "setup.py", *python_args }
       end
-    end
-    python_fortran_args = ["build", "--fcompiler=gfortran", *python_args]
-    %w[numpy scipy].each do |r|
-      resource(r).stage { system "python", "setup.py", *python_fortran_args }
-    end
+        resource("matplotlib").stage do
+          if MacOS.version >= :yosemite
+            inreplace "setupext.py", "'freetype2', 'ft2build.h',", "'freetype2', 'freetype2/ft2build.h',"
+          end
+          system "python", "setup.py", *python_args
+        end
+      python_fortran_args = ["build", "--fcompiler=gfortran", *python_args]
+      %w[numpy scipy].each do |r|
+        resource(r).stage { system "python", "setup.py", *python_fortran_args }
+      end
 
-    if build.with? "docs"
-      resource("docutils").stage do
-        system "python", "setup.py", "install", "--prefix=#{buildpath}"
+      if build.with? "docs"
+        resource("docutils").stage do
+          system "python", "setup.py", "install", "--prefix=#{buildpath}"
+        end
       end
     end
 
     mkdir "build" do
-      ENV["CMAKE_C_COMPILER"] = "#{ENV.cc}"
-      ENV["CMAKE_CXX_COMPILER"] = "#{ENV.cxx}"
-
       args = %W[
         -DCMAKE_PREFIX_PATH=#{prefix}
         -DENABLE_DOXYGEN=Off
+        -DCMAKE_C_COMPILER=#{ENV.cc}
+        -DCMAKE_CXX_COMPILER=#{ENV.cxx}
       ]
-      if build.with? "brewed-python"
-        args << "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'"
-      end
+
+      args << "-DPYTHON_EXECUTABLE='#{%x(python-config --prefix).chomp}/bin/python'"
+      args << "-DPYTHON_INCLUDE_DIR='#{%x(python-config --prefix).chomp}/include/python2.7'"
+      args << "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'"
 
       if build.with? "docs"
-        args << "-DSPHINX_EXECUTEABLE=#{buildpath}/bin/rst2html.py"
+        if build.with? "python-deps"
+          args << "-DSPHINX_EXECUTEABLE=#{buildpath}/bin/rst2html.py"
+        end
+        args << "-DENABLE_SPHINX=OFF"
       else
         args << "-DENABLE_SPHINX=OFF"
       end
@@ -115,7 +126,6 @@ class Gnuradio < Formula
       system "cmake", "..", *args, *std_cmake_args
       system "make"
       system "make install"
-
       inreplace "#{prefix}/etc/gnuradio/conf.d/grc.conf" do |s|
         s.gsub! "#{prefix}/", "#{HOMEBREW_PREFIX}/"
       end
